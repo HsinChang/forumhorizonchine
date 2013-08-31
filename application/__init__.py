9b7d763722ae87563491ed4be7a146ca9e9cd5fa
Tianming.Lu <leo@leo-pc>
2013-08-31 23:52:27 +0200

    use flask_login and flask_babel

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 from gae_mini_profiler import profiler, templatetags
 from werkzeug.debug import DebuggedApplication
 from flask_babel import Babel
+from flask_login import LoginManager, current_user
+from models import ExhibitorModel
 
 app = Flask('application')
 app.config.from_object('application.settings')
@@ -15,6 +17,7 @@ app.config.from_object('application.settings')
 # Enable jinja2 loop controls extension
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.jinja_env.globals['LANGUAGES'] = app.config['LANGUAGES']
+app.jinja_env.globals['current_user'] = current_user
 
 #Babel
 babel = Babel(app)
@@ -29,6 +32,19 @@ def get_locale():
 def get_timezone():
     if 'timezone' in session:
         return session['timezone']
+
+#login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    if userid == None:
+        return None
+    id = int(userid)
+    member = ExhibitorModel.get_by_id(id)
+    return member
+
 
 @app.context_processor
 def inject_profiler():

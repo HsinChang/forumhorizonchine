2df6c0be171b763e497f67f1136a55e62b5a4fc9
Tianming.Lu <leo@leo-pc>
2013-08-31 23:51:05 +0200

    add new models and forms

--- application/forms.py
+++ application/forms.py
@@ -12,15 +12,32 @@ from flaskext import wtf
 from flaskext.wtf import validators
 from wtforms.ext.appengine.ndb import model_form
 
-from .models import ExampleModel
+from .models import ExhibitorModel
 
+def username_exist_check(form, field):
+    """check if the username registered is already userd"""
+    username = field.data
+    member = ExhibitorModel.query(ExhibitorModel.username==username).get()
+    if member:
+        raise validators.ValidationError('username exists, choose a different one!')
 
-class ClassicExampleForm(wtf.Form):
-    example_name = wtf.TextField('Name', validators=[validators.Required()])
-    example_description = wtf.TextAreaField('Description', validators=[validators.Required()])
+class LoginForm(wtf.Form):
+    """form userd for login"""
+    username = wtf.TextField('Username', validators=[validators.InputRequired()])
+    password = wtf.PasswordField('Password', validators=[validators.InputRequired()])
 
-# App Engine ndb model form example
-ExampleForm = model_form(ExampleModel, wtf.Form, field_args={
-    'example_name': dict(validators=[validators.Required()]),
-    'example_description': dict(validators=[validators.Required()]),
-})
+class RegisterForm(wtf.Form):
+    """form user for registering and creating new member"""
+    username = wtf.TextField('Username', validators=[validators.InputRequired(), username_exist_check])
+    password = wtf.PasswordField('Password', validators=[validators.InputRequired()])
+    password_confirm = wtf.PasswordField('Confirm password', validators=[validators.EqualTo('password')])
+#    role = wtf.SelectField('Role', validators=[validators.InputRequired()], choices=[('ADMIN', 'ADMIN'), ('MEMBER', 'MEMBER')])
+#    first_name = wtf.TextField('First name', validators=[validators.InputRequired()])
+#    last_name = wtf.TextField('Last name', validators=[validators.InputRequired()])
+    email = wtf.TextField('Email', validators=[validators.Email()])
+    entreprise = wtf.TextField('Entreprise', validators=[validators.InputRequired()])
+
+class JobForm(wtf.Form):
+    title = wtf.TextField('Title', validators=[validators.InputRequired()])
+    type = wtf.SelectField('Type', choices=[('job', 'job'), ('internship', 'internship')])
+    content = wtf.TextAreaField('Content', validators=[validators.InputRequired()])
--- application/models.py
+++ application/models.py
@@ -8,10 +8,30 @@ App Engine datastore models
 
 from google.appengine.ext import ndb
 
+OFFRE_TYPES = {'JOB': 'job', 'INTERN': 'internship'}
 
-class ExampleModel(ndb.Model):
-    """Example Model"""
-    example_name = ndb.StringProperty(required=True)
-    example_description = ndb.TextProperty(required=True)
-    added_by = ndb.UserProperty()
-    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+class JobModel(ndb.Model):
+    title = ndb.StringProperty(required=True)
+    type = ndb.StringProperty(required=True)
+    poster = ndb.KeyProperty(required=True)
+    date = ndb.DateProperty(auto_now_add=True)
+    content = ndb.TextProperty()
+
+
+class ExhibitorModel(ndb.Model):
+    username = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    entreprise = ndb.StringProperty(required=True)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.key.id())

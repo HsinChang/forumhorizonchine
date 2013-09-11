bc041207932d48283a2869478ef1ac56ce386e2d
Tianming.Lu <leo@leo-pc>
2013-09-11 21:44:07 +0200

    use my own login&admin test function

--- application/decorators.py
+++ application/decorators.py
@@ -7,18 +7,19 @@ Decorators for URL handlers
 
 from functools import wraps
 from google.appengine.api import users
-from flask import redirect, request, abort
-
+from flask import redirect, request, abort, flash, url_for
+import flask_login as login
 
 def login_required(func):
     """Requires standard login credentials"""
     @wraps(func)
     def decorated_view(*args, **kwargs):
-        if not users.get_current_user():
-            return redirect(users.create_login_url(request.url))
+        user = login.current_user
+        if user.is_anonymous():
+            flash('login first!')
+            return redirect(url_for('exhibitor.index'))
         return func(*args, **kwargs)
     return decorated_view
-
 
 def admin_required(func):
     """Requires App Engine admin credentials"""

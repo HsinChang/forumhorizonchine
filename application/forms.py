b25de6842f758244d3fb521ab639ceecee31cf70
Tianming.Lu <lutianming1005@gmail.com>
2013-09-22 23:03:36 +0200

    start making job-edit support 3 languages

--- application/forms.py
+++ application/forms.py
@@ -9,9 +9,8 @@ See: http://flask.pocoo.org/docs/patterns/wtforms/
 """
 
 from flaskext import wtf
-from flaskext.wtf import validators
-from wtforms.ext.appengine.ndb import model_form
-
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField, FormField, SelectMultipleField
+from wtforms import validators
 from .models import UserModel, EnterpriseModel
 
 def username_exist_check(form, field):
@@ -29,33 +28,54 @@ def enterprise_exist_check(form, field):
 
 class LoginForm(wtf.Form):
     """form userd for login"""
-    username = wtf.TextField('Username', validators=[validators.InputRequired()])
-    password = wtf.PasswordField('Password', validators=[validators.InputRequired()])
+    username = StringField('Username', validators=[validators.InputRequired()])
+    password = PasswordField('Password', validators=[validators.InputRequired()])
 
 class RegisterForm(wtf.Form):
     """form user for registering and creating new member"""
-    username = wtf.TextField('Username', validators=[validators.InputRequired(), username_exist_check])
-    password = wtf.PasswordField('Password', validators=[validators.InputRequired()])
-    password_confirm = wtf.PasswordField('Confirm password', validators=[validators.EqualTo('password')])
+    username = StringField('Username', validators=[validators.InputRequired(), username_exist_check])
+    password = PasswordField('Password', validators=[validators.InputRequired()])
+    password_confirm = PasswordField('Confirm password', validators=[validators.EqualTo('password')])
 #    role = wtf.SelectField('Role', validators=[validators.InputRequired()], choices=[('ADMIN', 'ADMIN'), ('MEMBER', 'MEMBER')])
 #    first_name = wtf.TextField('First name', validators=[validators.InputRequired()])
 #    last_name = wtf.TextField('Last name', validators=[validators.InputRequired()])
-    email = wtf.TextField('Email', validators=[validators.Email()])
-    company = wtf.TextField('Company', validators=[validators.InputRequired()])
+    email = StringField('Email', validators=[validators.Email()])
+    company = StringField('Company', validators=[validators.InputRequired()])
+
+
+class JobMetaForm(wtf.Form):
+    publish = BooleanField('publish')
+    title = StringField('Title')
+    content = TextAreaField('Content')
+    def __init__(self, *args, **kwargs):
+        kwargs['csrf_enabled'] = False
+        super(JobMetaForm, self).__init__(*args, **kwargs)
 
 
 class JobForm(wtf.Form):
-    title = wtf.TextField('Title', validators=[validators.InputRequired()])
-    type = wtf.SelectField('Type', choices=[('job', 'job'), ('internship', 'internship')])
-    enterprise_mail = wtf.SelectField('Enterprise_Mail',)
-    content = wtf.TextAreaField('Content', validators=[validators.InputRequired()])
+    type = SelectField('Type', choices=[('job', 'job'), ('internship', 'internship')])
+    enterprise_mail = SelectField('Enterprise_Mail')
+    # en = FormField(JobMetaForm, 'en')
+    # fr = FormField(JobMetaForm, 'fr')
+    # zh = FormField(JobMetaForm, 'zh')
+    publish_en = BooleanField('Publish')
+    publish_zh = BooleanField('Publish')
+    publish_fr = BooleanField('Publish')
+    title_en = StringField('Title')
+    title_zh = StringField('Title')
+    title_fr = StringField('Title')
+    content_en = TextAreaField('Content')
+    content_zh = TextAreaField('Content')
+    content_fr = TextAreaField('Content')
+
+    cv_required = SelectMultipleField('required CV', choices=[('en', 'en'), ('fr', 'fr'), ('zh', 'zh')])
 
 
 class EnterpriseForm(wtf.Form):
-    name = wtf.TextField('Name', validators=[validators.InputRequired(), enterprise_exist_check])
-    shortname = wtf.TextField('Short name', validators=[validators.InputRequired()])
+    name = StringField('Name', validators=[validators.InputRequired(), enterprise_exist_check])
+    shortname = StringField('Short name', validators=[validators.InputRequired()])
     #at lest one email
-    email = wtf.TextField('Email', validators=[validators.Email()])
+    email = StringField('Email', validators=[validators.Email()])
 
 class EmailForm(wtf.Form):
-    email = wtf.TextField('Email', validators=[validators.Email()])
+    email = StringField('Email', validators=[validators.Email()])
